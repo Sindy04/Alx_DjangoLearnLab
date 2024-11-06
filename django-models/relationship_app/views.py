20e8f0bfe7a31aed627abcb3aecbe618c3c75a92
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-06 12:32:27 +0200

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,3 +11,10 @@
 "from django.contrib.auth.forms import UserCreationForm"
 
 "views.register","LogoutView.as_view(template_name=","LoginView.as_view(template_name="
+
+#Admin view
+"from django.contrib.auth.decorators imports permission_required"
+"from django.shortcuts import render"
+@permission_required('myapp.is_admin')
+def admin_view(request):
+  return render(request, 'admin.html')
