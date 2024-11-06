f1822ad7e2d6ca894e2596d58d79659a439aacc5
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-06 23:08:27 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,7 +8,7 @@ views.register",
 "from . import views"
 
 urlpatterns = [
-    path('admin/',views.admin_view, name = 'admin_view'),
+    path('Admin/',views.Admin_view, name = 'Admin_view'),
     path('librarian/',views.librarian_view, name = 'librarian_view'),
     path('member/' ,views.member_view, name = 'member_view'),
 ]
