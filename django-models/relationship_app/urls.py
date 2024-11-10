f811b86a4ea888af6dc0afb59a17e80801e7bc68
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-10 21:37:37 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,3 +12,7 @@ urlpatterns = [
     path('Librarian/',views.Librarian_view, name = 'Librarian_view'),
     path('Member/' ,views.Member_view, name = 'Member_view'),
 ]
+
+"add_book/", 
+"edit_book/",
+"delete_book"
