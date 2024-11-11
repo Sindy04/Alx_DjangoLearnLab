4b3ce85c8c00d3e52cc137be6bee221a94d63e31
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-11 23:11:45 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,1 +1,12 @@
 "models.CharField", "max_length", "200", "title", "author"
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+#Class Custom user
+Class CustomUser(AbstractUser):
+date_of_birth = models.DateField(null=True, blank=True)
+profile_photo = models.ImageField(upload_to = 'profile_photos/', null=True, blank=True)
+
+def __str__(self):
+  return self . username
