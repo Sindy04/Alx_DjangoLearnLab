c36dcab97cd61129befd95f978cc8efbb48d2c2f
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-11 23:15:24 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 #Class Custom user
-Class CustomUser(AbstractUser):
+class CustomUser(AbstractUser):
 date_of_birth = models.DateField(null=True, blank=True)
 profile_photo = models.ImageField(upload_to = 'profile_photos/', null=True, blank=True)
 
