33feb4e3b5ae3c5e5120ea77bb788f95af3045da
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-11 23:24:13 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,6 +3,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 #Class Custom user
 class CustomUser(AbstractUser):
 date_of_birth = models.DateField(null=True, blank=True)
@@ -10,3 +11,14 @@ profile_photo = models.ImageField(upload_to = 'profile_photos/', null=True, blan
 
 def __str__(self):
   return self . username
+
+from django.contrib.auth.models import BaseUserManager 
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+  def create_user(self, username, email, password=None, **extra_fields):
+    if not username:
+      raise ValueError('Users must have a username')
+      if not email:
+        raise ValueError('Users must have an email address')
+                         
