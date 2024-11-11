d44c9d7085453b3c40bcd134c10641d73d67a9c6
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-11 23:56:34 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,3 +2,21 @@
 "register", "admin.ModelAdmin"
 "list_filter", "author", "publication_year"
 "search_fields", "title"
+
+#Model Admin
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+  model = CustomUser
+  list_display = ('username', 'email', 'date_of_birth', 'profile_photo')
+  fieldsets = UserAdmin.fieldsets + (
+    (None, {'fields': ('date_of_birth','profile_photo')}),
+  )
+  add_fieldsets = UserAdmin.add_fieldsets +(
+    (None, {'fields': ('date_of_birth','profile_photo')})'
+  )
+
+admin.site.register(CustomUser, CustomUserAdmin)
