aa015919f7030aae5fc11eeedd0846d149b4af89
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-12 15:43:58 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,5 +4,8 @@ relationship_app/library_detail.html,
 library, 
 from .models import Library
 from django.views.generic.detail import DetailView
+
 from django.contrib.auth import login "
 from django.contrib.auth.forms import UserCreationForm
+UserCreationForm()
+relationship_app/register.html
