725ca6ed6d7fa3c05f5657f0cf36a123590d2b13
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-12 09:12:37 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,1 +1,15 @@
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from .models import Book
 
+#Create your views here.
+
+@permission_required('bookshelf .can_view_book', raise_exception=True)
+def book_list(request):
+  books = Book.objects.all()
+  return render(request, 'book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create_book', raise_exception=True)
+def book_delete(request, pk)
+#Book deletion logic here
+return render(request, 'book_confirm_delete.html')
