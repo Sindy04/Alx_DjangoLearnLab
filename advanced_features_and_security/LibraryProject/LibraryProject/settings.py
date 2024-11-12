e5f117b3f13b60ed71647a2a0a5737720f67d837
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-12 09:27:36 +0200

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -5,3 +5,18 @@ class OtherModel(models.Model ):
   user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
   ...
   bookshelf.CustomUser
+
+#Set DEBUG to False in production DEBUG = False
+#Browser-side protections
+SECURE_BROWSER_XXS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = TRUE
+
+#Enforce HTTPS-only cookies
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+#Additional security settings
+SECURE_SSL_REDIRECT =True
+SECURE_HSTS_SECOND = 3600
+SECURE_HSTS_PRELOAD = True
