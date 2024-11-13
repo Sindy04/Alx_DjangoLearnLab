3d7ee53cde569052931332af8b1c061168036ea3
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-13 12:18:34 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -22,3 +22,18 @@ can_add_book, can_change_book, can_delete_book
 
 class UserProfile(models.Model):
   Admin, Member
+
+from django.db import models
+from django.contrib.auth.models import user
+
+ROLE_CHOICES = [
+  ('Admin','Admin')
+  ('Librarian','Librarian')
+  ('Member','Member')
+]
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  role = models.CharField(max_length=10, choices = ROLE_CHOICES)
+
+def __str__(self):
+  return self.user.username
