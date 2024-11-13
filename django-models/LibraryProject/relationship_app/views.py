efaa9540714705f49687bb06150a56e6fd0e9758
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-13 12:44:53 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,4 +31,14 @@ def member_view(request):
 #Role checks
 def is_admin(user):
   return user.userprofile.role =='Admin'
-  
+
+def is_librabrian(user):
+  return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+  return user.userprofile.role == 'Member'
+
+#Apply role checks to views
+admin_view = user_passes_test(is_admin)(admin_view)
+librarian_view = user_passes_test(is_librarian)(librarian_view)
+member_view = user_passes_test(is_member)(member_view)
