b6bdb5689f1e5e085d94faabf16a7eee3494a23d
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-13 12:30:59 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,3 +14,21 @@ from django.contrib.auth.decorators import permission_required
 relationship_app.can_add_book
 relationship_app.can_change_book
 relationship_app.can_delete_book
+
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+def admin_view(request):
+  return render(request,'admin.html')
+
+def librarian_view(request):
+  return render(request, 'librarian.html')
+
+def member_view(request):
+  return render(request, 'member.html')
+
+#Role checks
+def is_admin(user):
+  return user.userprofile.role =='Admin'
+  
