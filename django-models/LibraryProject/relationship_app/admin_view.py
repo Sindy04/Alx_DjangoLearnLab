9ec8a1ebd126eb8907d8e7162df88b800f4f9c86
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-14 08:46:43 +0200

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -5,3 +5,8 @@ admin_view = user_passes_test(is_admin)(admin_view)
 def admin_view(request)
 #your view logic
 return render(*args,*kwargs)
+
+@receiver(post_save,sender=User)
+def create_user_profile(sender,instance,created,**kwargs):
+  if created:
+    UserProfile.objects.create(user=instance)
