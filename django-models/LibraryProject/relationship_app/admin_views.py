d7e57a0605d678f1e1e3ba17af8268c68dc4fffc
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-16 22:05:35 +0200

    Update and rename admin_view.py to admin_views.py

--- django-models/LibraryProject/relationship_app/admin_views.py
+++ django-models/LibraryProject/relationship_app/admin_views.py
@@ -1,6 +1,6 @@
 def is_admin()
   #test logic
-@user_passes_test(admin_test)
+@users_passes_test(admin_test)
 admin_view = user_passes_test(is_admin)(admin_view)
 def admin_view(request)
 #your view logic
