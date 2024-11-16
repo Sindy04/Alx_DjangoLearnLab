fd7beca80e0b5b77f5e9c817b58fe79021f498a9
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-16 21:29:32 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -45,6 +45,7 @@ member_view = user_passes_test(is_member)(member_view)
 
 def is_admin()
   #test logic
+@users_passes_test
 @user_passes_test(admin_test)
 admin_view = user_passes_test(is_admin)(admin_view)
 def admin_view(request)
