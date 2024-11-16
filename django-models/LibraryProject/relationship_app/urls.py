bf6292a72f8e01e709ae13893120963f1e04e8a1
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-16 22:22:01 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,3 +8,10 @@ LoginView.as_view(template_name=
 add_book/
 edit_book/
 delete_book
+                  
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('admin/'views.admin_view,name ='admin_view')'
+]
