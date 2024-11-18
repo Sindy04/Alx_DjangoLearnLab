a2a3d52355906222ada42e66dbbf852eef49a1b4
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-18 09:23:17 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import MyModel
 from .serializers import MyModelSerializer
-
+generics.ListAPIView
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
