7dd39dfc7f07a2c4cba942cbde532c1a2fb31716
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-18 09:28:44 +0200

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,6 +1,6 @@
 pythonCopy codefrom django.urls import path
 from .views import BookListCreateAPIView
-
+api.urls
 urlpatterns = [
     path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
 ]
