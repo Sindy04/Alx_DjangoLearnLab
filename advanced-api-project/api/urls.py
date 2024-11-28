86afd2c89aa4627a7128bee037dfd8cd4b42faf4
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-28 10:11:09 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('books/',views.BookCreateView.as_view(),name='book-create'),
   path('books/',views.BookCreate.as_view(),name= 'books/create'),
   path('books/',views.Bookupdate.as_view(0,name='books/update'),
+  path('books/',views.Bookdelete.as_view(0,name='books/delete')
 ]
