4dfdbff2c0e85120d3c51e9bb16949ed77cb7714
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-28 09:06:03 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,1 +1,12 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+class BookDetailView(APIView):
+  def get(self, resquest, pk):
+    #Logic to retrieve a book by ID goes here
+    book = Book.objects.get(pk=pk)
+    serializer = BookSerializer(book)
+    return
+    Response(serializer.data)
+
+class
