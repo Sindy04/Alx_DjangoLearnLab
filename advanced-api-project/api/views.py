5343ca124957242883a92c2bd2a37088bfc0d853
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-11-30 07:32:17 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -45,3 +45,5 @@ class UserListView(generics.ListAPIView):
     filter_backends = [filters.SearchFilter]
     search_fields = ['username', 'email']
   filters.SearchFilter
+
+title, author, publication_year
