ff0b1a674fc141cb25d7305151bfa7d3b1a409b9
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-06 11:20:25 +0200

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -2,3 +2,13 @@ INSTALLED_APPS = [
   ....
   'blog', Add this line
 ]
+DATABASES={
+  'default':{
+  'ENGINE': 'django.db.backends.postgresql', #Use 'postgresql' for PostgreSQL
+    'NAME': 'database_name', #Replace with your database name
+    'USER': 'database_user', #Replace with your databse username
+    'PASSWORD': 'database_password', #Replace with your database password
+    'HOST':'localhost', #Replace with your database host
+    'PORT':'5432', #Replace with your database port
+  }
+}
