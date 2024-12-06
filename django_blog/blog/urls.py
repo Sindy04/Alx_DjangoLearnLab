0a9ce7f471d0b0bc4ac17ec7dbb37d41cb27bbfb
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-06 10:16:10 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,3 +7,13 @@ urlpatterns = [
   path('register/',views.register_view, name='register')
   path('profile/',views.register_view, name='profile')
   
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('posts/', views.PostListView.as_view(), name='post_list'),
+  path('posts/<int:pk>/',views.PostDetailView.as_view(), name='post_detail'),
+  path('posts/new/',views.PostCreateView.as_view(), name='post_create'),
+  path('posts/<int:pk>/edit/',views.PostUpdateView.as_view(), name='post_update'),
+  path('posts/<int:pk>/delete/',views.PostDeleteView.as_view(), name ='post_delete'),
+]
