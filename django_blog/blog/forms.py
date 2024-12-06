9c3786e1283c8cb159086237d22c99f313304852
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-06 11:42:26 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,1 +1,13 @@
+from django import forms
+from taggit.forms import TagWidget
+from .models import Post
 
+class PostForm(forms.ModelForm):
+  tags = forms.CharField(required=False, widget=TagWidget)
+
+class Meta:
+  model = Post
+  fields = ['title', 'content']
+  widgets ={
+    'tags': TagWidget,
+  }
