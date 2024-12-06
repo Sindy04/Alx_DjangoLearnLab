d533542d542f119ee18dd42d419b55327d91032b
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-06 18:14:35 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,9 +17,11 @@ class Comment(models.Model):
   post = models.ForeignKey(Post,on_delete=models.CASCADE)
   author = models.ForeignKey(User,on_delete = models.TextField()
   created_at = models.DateTimeField(auto_now=True)
+updated_at = models.DateTimeField(auto_now=True)
 
 def __str__(self):
   return f'Comment by {self.author.username} on {self.post.title}'
+
 
 
 
