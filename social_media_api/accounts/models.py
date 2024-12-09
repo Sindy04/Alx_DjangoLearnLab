f0002529960eeb5455281dc30768ff2cf0a1581b
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-09 10:10:42 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,3 +6,10 @@ class User(AbstractUser):
   profile_picture = models.ImageField(upload_to='profile_pictures/',blank=True)
   followers= models.ManyToManyField('self',symmetrical=False,blank=True)
   
+#Implementing User Follows and Feed Functionality
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+  following = models.ManyToManyField('self',symmetrical=False, blank=True)
+  
