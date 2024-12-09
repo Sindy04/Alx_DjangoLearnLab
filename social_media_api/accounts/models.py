e281f2f5d9fa02d42855d742857a04c1f0ff52de
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-09 08:15:37 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,1 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class User(AbstractUser):
+  bio = models.TextField(blank=True)
+  profile_picture = models.ImageField(upload_to='profile_pictures/',blank=True)
+  followers= models.ManyToManyField('self',symmetrical=False,blank=True)
+  
