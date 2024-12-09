67082c86b5b95f27cca26917844b8518dc865455
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-09 08:23:19 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework.authtoken.models import Token
 
 class UserSerializers(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ['id', 'username', 'bio', 'profile_picture']
+
+serializers.CharField()
+Token.objects.create
+get_user_model().objects.create_user
