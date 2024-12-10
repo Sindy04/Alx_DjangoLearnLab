d4afccc782955f29f938196a12ac1c4a42f3a6ce
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-10 09:14:26 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -9,3 +9,22 @@ INSTALLED_APPS=[
   'rest_framework',
   'posts',
 ]
+
+DEBUG = False
+ALLOWED_HOSTS
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'your-database-name',
+    'USER': 'your-database-user',
+    'PASSWORD': 'your-database-password'
+    'HOST':  'your-database-host'
+    'PORT': '5432'
+  }
+}
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
+  
