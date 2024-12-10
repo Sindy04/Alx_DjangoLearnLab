a348f85b2e6b5b74247cb97712633dbf9bf37b34
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-12-10 08:47:13 +0200

    Update models.py

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,1 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
+class Notification(models.Model):
+  recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name
